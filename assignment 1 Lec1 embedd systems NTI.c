2a0a771f4d9a546d18cdccb7a7dcf89124fb5e3f
Ghaleb445 <elsamerymohamed445@gmail.com>
2026-07-21 10:43:04 +0300

    assignment 1 Lec1 embedd systems NTI.c

--- assignment 1 Lec1 embedd systems NTI.c
+++ assignment 1 Lec1 embedd systems NTI.c
@@ -6,13 +6,13 @@ int main()
 
     for (int i = 1; i <= rows; i++)
     {
-        // Print spaces
+        
         for (int j = 1; j <= rows - i; j++)
         {
             printf(" ");
         }
 
-        // Print X's
+       
         for (int j = 1; j <= (2 * i - 1); j++)
         {
             printf("x");
