f1ec1559d08c75ef14cad26e613be8346e139e9f
Ghaleb445 <elsamerymohamed445@gmail.com>
2026-07-21 10:44:41 +0300

    Memory access project.c

--- Memory access project.c
+++ Memory access project.c
@@ -13,11 +13,7 @@ typedef signed long s64;
 const u8 MAX_SPEED = 255;
 
 void PrintSensorValue(const u16 *val) {
-	/*
-	 * Doesn't work because the value pointed
-	 * to by "val" is marked as const.
-	 * *val = 0;
-	 */
+
 
 	printf("Reading Sensor... Temperature is: %hu\n", *val);
 }
@@ -30,23 +26,14 @@ int main(void) {
 	u8 *const hardware_ptr = &PORTA_DATA;
 
 	printf("System Booting...\n");
-	/*
-	 * Doesn't work because MAX_SPEED is
-	 * declared as const.
-	 * MAX_SPEED = 200;
-	 */
+	
 
 	printf("Max Speed Configuration: %hu\n", MAX_SPEED);
 	PrintSensorValue(&Temperature);
 
 	*hardware_ptr = 100;
 	printf("Hardware Port Data is now: %hu\n", PORTA_DATA);
-	/*
-	 * Doesn't work because hardware_ptr is
-	 * a constant pointer pointing at variable
-	 * data.
-	 * hardware_ptr = &MAX_SPEED;
-	 */
+	
 
 	printf("Device ID verified: %d\n", *id_ptr);
 }
