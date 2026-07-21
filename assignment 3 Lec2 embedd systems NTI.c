597784c9dc56dc9d3bb680a962b6e7a7273c8d20
Ghaleb445 <elsamerymohamed445@gmail.com>
2026-07-21 10:44:16 +0300

    assignment 3 Lec2 embedd systems NTI.c

--- assignment 3 Lec2 embedd systems NTI.c
+++ assignment 3 Lec2 embedd systems NTI.c
@@ -9,13 +9,13 @@ int main()
 
     for (int i = 1; i <= height; i++)
     {
-        // Print spaces
+        
         for (int j = 1; j <= height - i; j++)
         {
             printf(" ");
         }
 
-        // Print stars
+        
         for (int j = 1; j <= (2 * i - 1); j++)
         {
             printf("*");
