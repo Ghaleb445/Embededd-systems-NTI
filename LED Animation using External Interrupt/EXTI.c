188bef0b24b01a9c5e183d111e4a7c664d141a79
Ghaleb445 <elsamerymohamed445@gmail.com>
2026-07-27 17:18:44 +0300

    Create EXTI.c

---
+++ LED Animation using External Interrupt/EXTI.c
@@ -0,0 +1,34 @@
+#include <avr/io.h>
+
+#include "STD_TYPES.h"
+#include "BIT_MATH.h"
+#include "EXTI.h"
+
+void EXTI_INT0_Init(u8 Trigger)
+{
+	switch(Trigger)
+	{
+		case LOW_LEVEL:
+		CLR_BIT(MCUCR,ISC00);
+		CLR_BIT(MCUCR,ISC01);
+		break;
+
+		case IOC:
+		SET_BIT(MCUCR,ISC00);
+		CLR_BIT(MCUCR,ISC01);
+		break;
+
+		case FALLING_EDGE:
+		CLR_BIT(MCUCR,ISC00);
+		SET_BIT(MCUCR,ISC01);
+		break;
+
+		case RISING_EDGE:
+		SET_BIT(MCUCR,ISC00);
+		SET_BIT(MCUCR,ISC01);
+		break;
+	}
+
+	/* Enable INT0 */
+	SET_BIT(GICR,INT0);
+}
